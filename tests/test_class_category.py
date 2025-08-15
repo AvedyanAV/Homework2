4d626b7b656d5d30cf31de49a39bedc5a4338ca2
Amajk <Amajk577@mail.ru>
2025-08-15 17:08:11 +0300

    add @property

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -8,7 +8,7 @@ def sample_products():
     return [
         Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
         Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-        Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)  # Дубликат по имени
+        Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
     ]
 
 
@@ -26,4 +26,19 @@ def test_category_initialization(sample_category, sample_products):
 
 def test_total_categories_count(sample_category):
     """Проверяем подсчет количества категорий"""
-    assert Category.category_count >= 1  # Как минимум наша категория
+    assert Category.category_count >= 1
+
+
+def test_products_property(sample_category):
+    """Проверка работы геттера"""
+    products_info = sample_category.products
+    assert len(products_info) == 3
+    assert products_info[0] == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert products_info[1] == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+    assert products_info[2] == "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+
+
+
+
+
+
